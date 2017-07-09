74d9d4a385aca1a2934f2127bf25fd0bcbf1662c
dwqs <461147874@qq.com>
2017-07-10 00:19:24 +0800

    [misc]更新构建方式

---
+++ src/general/wiki/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wiki" id="wiki">
+        这是 wiki extension
+    </div>
+</template>
+
+<script>
+
+    import Setting from '../setting/index.vue';
+    import AddLink from '../add-link/index.vue';
+
+    export default {
+        components: {
+            Setting,
+            AddLink
+        }
+    }
+</script>
--- src/page/index.js
+++ src/page/index.js
@@ -3,33 +3,12 @@
  */
 
 import './reset.less';
-import 'babel-polyfill';
 
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 
-Vue.use(VueRouter);
-
-const Setting = () => import(/* webpackChunkName: "set" */ '../general/setting/');
-const AddLink = () => import(/* webpackChunkName: "add" */ '../general/add-link/');
-
-const Outer = { template: '<router-view></router-view>' };
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            component: Outer,
-            children: [
-                { path: '', component: Setting, alias: 'index' },
-                { path: 'add', component: AddLink }
-            ]
-        }
-    ]
-});
+import Wiki from '../general/wiki/index.vue';
 
 new Vue({
-    router,
-    ...Outer
-}).$mount('#wiki');
+    el: '#wiki',
+    render: (h) => h(Wiki)
+});
